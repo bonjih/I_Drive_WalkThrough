f40cd568fddac74917fa8c6fbee7da92ae139f4f
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2019-01-14 11:23:29 +1000

    Update I_Drive_WalkThrough.py
    removed file created and modified columns for faster walk.

--- I_Drive_WalkThrough.py
+++ I_Drive_WalkThrough.py
@@ -1,21 +1,21 @@
+import sqlalchemy
 from sqlalchemy import create_engine
-import mysql.connector
+import pymysql
 import os
 import pandas as pd
 import time
 
 start_time = time.time()
 
-conn = mysql.connector.connect(host='xxx', database='drive_indexing', user='xxxx', password = 'xxxx')
+conn = pymysql.connect(user='debian-sys-maint', passwd='SoLcxvfgbzqU0ixI', host='10.6.66.160', database='drive_indexing')
 cursor = conn.cursor()
 
-engine = create_engine('mysql+mysqldb://xxxx:xxxxx@xxxxx/drive_indexing?charset=utf8', encoding = 'utf-8')
+engine = create_engine('mysql+pymysql://debian-sys-maint:SoLcxvfgbzqU0ixI@10.6.66.160/drive_indexing?charset=utf8')
 
 paths = 'I:\\1. Projects'
 
 query_drop_table = ("""DROP TABLE IF EXISTS I_dir_name""")
 cursor.execute (query_drop_table)
-conn.commit()
 
 query_create_dir_name = """CREATE TABLE I_dir_name(_id int(10) primary key AUTO_INCREMENT, 
                 Client varchar (155))"""
@@ -33,22 +33,25 @@ for i in dirlist:
         cursor.executemany(dir_name_query, dir_list)
         counter +=1
 conn.commit()
-conn.close()
+
+
+query_drop_I_drive = ("""DROP TABLE IF EXISTS I_drive""")
+cursor.execute(query_drop_I_drive)
 
 data = list()
 for root, dirs, files in os.walk(paths):
     try:
         for filename in files:
             nm, ext = os.path.splitext(filename)
-            ctimes = time.ctime(os.stat(paths).st_ctime)
-            mtimes = time.ctime(os.stat(paths).st_mtime)
-            fullpath = os.path.join(os.path.abspath(root), filename)
-            data.append((filename, fullpath, ctimes, mtimes))
+            # ctimes = time.ctime(os.stat(paths).st_ctime)
+            # mtimes = time.ctime(os.stat(paths).st_mtime)
+            fullpath = os.path.join(os.path.abspath(root))
+            data.append((filename, fullpath))
             # print(data)
     except OSError:
         pass
 
-df = pd.DataFrame(data, columns=['Filename', 'Fullpath', 'Created', 'Modified'])
+df = pd.DataFrame(data, columns=['Filename', 'Fullpath'])
 df.to_sql(con=engine, name='I_drive', if_exists='replace', index=True, index_label='id')
 
 // dtype=sqlalchemy.types.NVARCHAR(length=255) converts columns to 'utf8_general_ci' if run into 
